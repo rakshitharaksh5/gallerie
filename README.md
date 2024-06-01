255340ef767c1e09e026510f8664c24255fdcfef
rakshitharaksh5 <rakshitharakshi296@gmail.com>
2024-06-01 21:18:56 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Gallerie - A photo Search App
+
+## Langauges Used
+
+- HTML
+- CSS
+- JavaScript
+
+## API used
+- Unsplash API
+
+## Color Palette
+
---
+++ assets/common.js
@@ -0,0 +1,12 @@
+// Access key
+const API_KEY = "Ziu_zJUX-5bI0CfcnYim_VYIga4O8kr7p8DIZf_qyp4";
+
+function convert_to_json(data) {
+    return data.json();
+}
+
+//Secret key
+// XK4EJ0SGEncLl951nxPh91puUYmYfnX5pLrsXxWt4y8
+
+// Application ID
+// 618105
