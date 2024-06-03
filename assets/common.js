b5e6a26e68aeaea169ef7e3060926db4c8f30cf4
Rakshitha Anand <138316028+rakshitharaksh5@users.noreply.github.com>
2024-06-03 12:27:43 +0530

    Update common.js

--- assets/common.js
+++ assets/common.js
@@ -1,5 +1,5 @@
 // Access key
-const API_KEY = "**************************";
+const API_KEY = "Ziu_zJUX-5bI0CfcnYim_VYIga4O8kr7p8DIZf_qyp4";
 
 function convert_to_json(data) {
     return data.json();
